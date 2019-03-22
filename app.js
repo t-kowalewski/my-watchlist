e098da304af5e3f60a57d99a65ce5df1b31932a8
t-kowalewski <wkowalewski@outlook.com>
2019-03-22 19:12:00 +0100

    add UI prototype & font awesome

--- app.js
+++ app.js
@@ -8,13 +8,33 @@ function Movie(title, director, imdb) {
 // UI Constructor
 function UI() {}
 
-// Add Movie to List
+// Prototype - Add Movie to List
 UI.prototype.addMovieToList = function(movie) {
-	// console.log('TCL: UI.prototype.addMovieToList -> movie', movie);
+  const list = document.querySelector('#movie-list');
   
+  // Create <tr> element
+  const row = document.createElement('tr');
+  
+  // Insert cols - <td>
+  row.innerHTML = `
+  <td>${movie.title}</td>
+  <td>${movie.director}</td>
+  <td>${movie.imdb}</td>
+  <td><i class="delete fas fa-times"></i></td>
+  `;
+
+  // Append to the List
+  list.appendChild(row);
 }
 
-// Event Listeners
+// Prototype - Clear Input
+UI.prototype.clearInput = function() {
+  document.querySelector('#title').value = '';
+  document.querySelector('#director').value = '';
+  document.querySelector('#imdb').value = '';
+}
+
+// Event Listeners ===============================
 document.querySelector('#movie-form').addEventListener('submit', function(e) {
 
   // Get form values
@@ -22,15 +42,17 @@ document.querySelector('#movie-form').addEventListener('submit', function(e) {
         director = document.querySelector('#director').value,
         imdb = document.querySelector('#imdb').value;
   
-  // Instantiate movie
+  // Instantiate Movie
   const movie = new Movie(title, director, imdb);
   
   // Instantiate UI
   const ui = new UI();
-	console.log('TCL: ui', ui);
 
-  // Add Movie to List
+  // UI - Add Movie to List
   ui.addMovieToList(movie);
+
+  // UI - Clear Input
+  ui.clearInput();
 
   e.preventDefault();
 });
