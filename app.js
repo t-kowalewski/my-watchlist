2fba2e138a355e979f3da23d0fbe595292737bfd
t-kowalewski <wkowalewski@outlook.com>
2019-03-23 02:21:47 +0100

    add showAlert prototype

--- app.js
+++ app.js
@@ -6,15 +6,15 @@ function Movie(title, director, imdb) {
 }
 
 // UI Constructor
-function UI() {}
+function UI() { }
 
 // Prototype - Add Movie to List
-UI.prototype.addMovieToList = function(movie) {
+UI.prototype.addMovieToList = function (movie) {
   const list = document.querySelector('#movie-list');
-  
+
   // Create <tr> element
   const row = document.createElement('tr');
-  
+
   // Insert cols - <td>
   row.innerHTML = `
   <td>${movie.title}</td>
@@ -27,32 +27,62 @@ UI.prototype.addMovieToList = function(movie) {
   list.appendChild(row);
 }
 
+// Prototype - Show Alert
+UI.prototype.showAlert = function (msg, alertClass) {
+  // Select form
+  const form = document.querySelector('#movie-form');
+  // Create <div>
+  const div = document.createElement('div');
+  // Add Class
+  div.classList.add('alert', alertClass);
+  // Add Text
+  // div.appendChild(document.createTextNode(msg));
+  div.innerText = `${msg}`;
+  // Add div inside form
+  form.prepend(div);
+
+  // Timeout after 3 sec
+  setTimeout(function () {
+    document.querySelector('.alert').remove();
+  }, 4000);
+}
+
 // Prototype - Clear Input
-UI.prototype.clearInput = function() {
+UI.prototype.clearInput = function () {
   document.querySelector('#title').value = '';
   document.querySelector('#director').value = '';
   document.querySelector('#imdb').value = '';
 }
 
 // Event Listeners ===============================
-document.querySelector('#movie-form').addEventListener('submit', function(e) {
+document.querySelector('#movie-form').addEventListener('submit', function (e) {
 
   // Get form values
   const title = document.querySelector('#title').value,
-        director = document.querySelector('#director').value,
-        imdb = document.querySelector('#imdb').value;
-  
+    director = document.querySelector('#director').value,
+    imdb = document.querySelector('#imdb').value;
+
   // Instantiate Movie
   const movie = new Movie(title, director, imdb);
-  
+
   // Instantiate UI
   const ui = new UI();
 
-  // UI - Add Movie to List
-  ui.addMovieToList(movie);
+  // Validate input
+  if (title === '' || director === '' || imdb === '') {
+    // Error Alert
+    ui.showAlert('Please fill in all fields', 'error');
+  }
+  else {
+    // UI - Add Movie to List
+    ui.addMovieToList(movie);
 
-  // UI - Clear Input
-  ui.clearInput();
+    // Show Alert
+    ui.showAlert('Movie successfully added!', 'success');
+
+    // UI - Clear Input
+    ui.clearInput();
+  }
 
   e.preventDefault();
 });
